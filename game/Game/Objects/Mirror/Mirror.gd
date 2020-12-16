01c5667ce58351883cd622c05ff3b58cb9529b26
Harrison Hall <harrisonchristianhall@gmail.com>
2020-12-16 17:04:00 -0500

    using data instead of game_handler is a NONO

--- game/Game/Objects/Mirror/Mirror.gd
+++ game/Game/Objects/Mirror/Mirror.gd
@@ -6,5 +6,4 @@ func _ready():
 func update():
 	var trans = $Viewport/CamOffset.get_global_transform()
 	trans.origin = get_global_transform().origin
-	#$Viewport/CamOffset.set_global_transform(trans)
 	$Viewport/CamOffset.set_global_transform(get_global_transform())
