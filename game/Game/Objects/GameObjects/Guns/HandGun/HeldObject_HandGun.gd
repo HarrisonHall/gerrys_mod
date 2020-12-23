5e7ffb179fe2a8d02a61e33a2599222c8eaec184
owlkeeper77 <gantash6@aol.com>
2020-12-22 21:12:32 -0500

    HandGun casing

--- game/Game/Objects/GameObjects/Guns/HandGun/HeldObject_HandGun.gd
+++ game/Game/Objects/GameObjects/Guns/HandGun/HeldObject_HandGun.gd
@@ -1,6 +1,7 @@
 extends "res://Game/Objects/GameObjects/Guns/HeldObject.gd"
 
-
+export (PackedScene) var casing = null
+export var initial_velocity = 10.0
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -11,8 +12,15 @@ func _init():
 	._init()
 	bullet_type = "BasicBullet"
 	bspeed = 200
+	bscale = .38
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func _emit_casing():
+	var new_casing = Game.make_obj("Casing")
+	new_casing.transform = $Pivot/CasingSpawn.global_transform
+	new_casing.mom = new_casing.transform.basis.z * initial_velocity
+	new_casing.send_update()
